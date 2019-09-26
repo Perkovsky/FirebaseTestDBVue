03beb8df91495e9e205762ef053da63e870a24ff
Igor Perkovsky <igor.perkovsky@mail.ru>
2019-09-26 23:53:00 +0300

    Added project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank">
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <div class="mt-9 ml-9">
+        <v-badge color="red" overlap>
+          <template v-slot:badge>
+            <span v-if="messages > 0">{{ messages }}</span>
+          </template>
+          <v-btn color="primary">Some button</v-btn>
+        </v-badge>
+      </div>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as fb from 'firebase'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data: () => ({
+    messages: -1
+  }),
+  methods: {
+    sendTestMessage () {
+      fb.database().ref('events')
+        .child('111-222-333')
+        .push({Id:'11111', Messages: 'some text'})
+    }
+  },
+  mounted () {
+    const self = this
+    // see: firebase -> access (rules)
+    fb.database().ref('events').on('value', function (snapshot) {
+      self.messages++
+    })
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as fb from 'firebase/app'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   render: h => h(App),
+  created () {
+    var firebaseConfig = {
+      apiKey: 'AIzaSyBZJHGXPg0yjVP5o-3MWVyhIROSykUnZiw',
+      authDomain: 'testdb-32619.firebaseapp.com',
+      databaseURL: 'https://testdb-32619.firebaseio.com',
+      projectId: 'testdb-32619',
+      storageBucket: 'testdb-32619.appspot.com',
+      messagingSenderId: '249322938490',
+      appId: '1:249322938490:web:1040dae601940eade85bc3'
+    }
+    // Initialize Firebase
+    fb.initializeApp(firebaseConfig);
+  }
 }).$mount('#app')
